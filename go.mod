dc8788067a5eaab5a9bef486eaba38c42ff8dc96
Pranjalya <pranjalyawarrior@gmail.com>
2020-09-27 22:09:44 +0530

    Initial commit :wrench:

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gituhb.com/Pranjalya/vue-go-graphql
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "gituhb.com/Pranjalya/vue-go-graphql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
